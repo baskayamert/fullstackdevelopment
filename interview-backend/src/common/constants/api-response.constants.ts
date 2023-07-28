af2afb07416d3b81304a5467db4c1ccd6e9f01d3
baskayamert <baskaya_1999@hotmail.com>
2023-07-28 21:51:38 +0300

    Api constants file was created for custom api response texts.

---
+++ interview-backend/src/common/constants/api-response.constants.ts
@@ -0,0 +1,6 @@
+export const API_RESPONSE_TEXTS = {
+    SUCCESS: 'Success',
+    NOT_FOUND: 'Resource not found',
+    BAD_REQUEST: 'Bad request',
+    INTERNAL_ERROR: 'Internal server error',
+  };
