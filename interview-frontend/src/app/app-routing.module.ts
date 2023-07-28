912ed0fafe94e5aeab273a47783e59057dfe1035
baskayamert <baskaya_1999@hotmail.com>
2023-07-28 22:12:00 +0300

    Routes were created.

--- interview-frontend/src/app/app-routing.module.ts
+++ interview-frontend/src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CityComponent } from './components/city/city.component';
+import { AppComponent } from './app.component';
+import { PageNumberParameterGuard } from './guards/page-number-parameter.guard';
+import { SearchTextParameterGuard } from './guards/search-text-parameter.guard';
+import { HomeComponent } from './components/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent},
+  { path: 'cities/searchResult/:searchText/:pageNumber', component: CityComponent, canActivate: [SearchTextParameterGuard]},
+  { path: 'cities/:pageNumber', component: CityComponent, canActivate: [PageNumberParameterGuard] },
+  { path: 'cities/searchResult/:searchText', redirectTo: '/cities/searchResult/:searchText/1'},
+  { path: 'cities', redirectTo: '/cities/1'},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [PageNumberParameterGuard, SearchTextParameterGuard]
 })
 export class AppRoutingModule { }
