81464b8fb8aab449f16eec8fd947753d7c1fc26d
baskayamert <baskaya_1999@hotmail.com>
2023-07-28 22:08:20 +0300

    Created components and used libraries were registered to app.module.

--- interview-frontend/src/app/app.module.ts
+++ interview-frontend/src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CityComponent } from './components/city/city.component';
+import { HttpClientModule } from '@angular/common/http';
+import { CityPaginationComponent } from './components/city/city-pagination/city-pagination.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CityComponent,
+    CityPaginationComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
