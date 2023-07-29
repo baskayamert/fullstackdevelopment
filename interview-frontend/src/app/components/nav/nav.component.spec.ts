d977e945edf1fbe3300adb065e19a1813c02ff9a
baskayamert <baskaya_1999@hotmail.com>
2023-07-30 02:04:31 +0300

    Text cases were written for the nav component.

--- interview-frontend/src/app/components/nav/nav.component.spec.ts
+++ interview-frontend/src/app/components/nav/nav.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { NavComponent } from './nav.component';
+import { HOME_TEXT } from 'src/app/common/constants/app.constants';
 
 describe('NavComponent', () => {
   let component: NavComponent;
@@ -18,4 +19,13 @@ describe('NavComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it("Title should be correct", () => {
+    expect(component.navbarHomeText).toEqual(HOME_TEXT);
+  })
+
+  it("Navbar should have the correct title in HTML", () => {
+    const data = fixture.nativeElement;
+    expect(data.querySelector('.navbar-brand').textContent).toContain(HOME_TEXT)
+  })
 });
