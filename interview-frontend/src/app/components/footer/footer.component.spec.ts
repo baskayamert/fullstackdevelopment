d3f47a0a643ee41729cb6e085ec5c6b31b02741a
baskayamert <baskaya_1999@hotmail.com>
2023-07-30 23:16:01 +0300

    Test cases were prepared for footer.component.

--- interview-frontend/src/app/components/footer/footer.component.spec.ts
+++ interview-frontend/src/app/components/footer/footer.component.spec.ts
@@ -4,28 +4,44 @@ import { FooterComponent } from './footer.component';
 import { FOOTER_TEXT } from 'src/app/common/constants/app.constants';
 
 describe('FooterComponent', () => {
-  let component: FooterComponent;
-  let fixture: ComponentFixture<FooterComponent>;
+  describe('FooterComponent HTML', () => {
+    let component: FooterComponent;
+    let fixture: ComponentFixture<FooterComponent>;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [FooterComponent]
+    beforeEach(() => {
+      TestBed.configureTestingModule({
+        declarations: [FooterComponent]
+      });
+      fixture = TestBed.createComponent(FooterComponent);
+      component = fixture.componentInstance;
+      fixture.detectChanges();
     });
-    fixture = TestBed.createComponent(FooterComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+
+    it('should create', () => {
+      expect(component).toBeTruthy();
+    });
+
+    it("should have the correct footer text", () => {
+      const data = fixture.nativeElement;
+      expect(data.querySelector('.footerText').textContent).toContain(FOOTER_TEXT)
+    });
+
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  describe('FooterComponent TypeScript', () => {
+    let component: FooterComponent;
+    beforeEach(() => {
+      component = new FooterComponent();
+    });
+
+    it('should create', () => {
+      expect(component).toBeTruthy();
+    });
+
+    it("should have the correct Footer Text", () => {
+      expect(component.footerText).toEqual(FOOTER_TEXT);
+    });
+
   });
 
-  it("Footer text should be correct", () => {
-    expect(component.footerText).toEqual(FOOTER_TEXT);
-  });
-
-  it("Footer should have the correct footer text in HTML", () => {
-    const data = fixture.nativeElement;
-    expect(data.querySelector('.footerText').textContent).toContain(FOOTER_TEXT)
-  });
 });
