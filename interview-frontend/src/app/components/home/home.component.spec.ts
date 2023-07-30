209acc9f5ff68f38367cff5054696141f4b6581d
baskayamert <baskaya_1999@hotmail.com>
2023-07-30 23:15:26 +0300

    Test cases were prepared for home.component.

--- interview-frontend/src/app/components/home/home.component.spec.ts
+++ interview-frontend/src/app/components/home/home.component.spec.ts
@@ -4,47 +4,65 @@ import { HomeComponent } from './home.component';
 import { HOMEPAGE_CLICK_TEXT, HOMEPAGE_SURPRISE_TEXT, HOMEPAGE_WELCOME_TEXT } from 'src/app/common/constants/app.constants';
 
 describe('HomeComponent', () => {
-  let component: HomeComponent;
-  let fixture: ComponentFixture<HomeComponent>;
-
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [HomeComponent]
+  describe('HomeComponent HTML', () => {
+    let component: HomeComponent;
+    let fixture: ComponentFixture<HomeComponent>;
+  
+    beforeEach(() => {
+      TestBed.configureTestingModule({
+        declarations: [HomeComponent]
+      });
+      fixture = TestBed.createComponent(HomeComponent);
+      component = fixture.componentInstance;
+      fixture.detectChanges();
     });
-    fixture = TestBed.createComponent(HomeComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+  
+    it('should create', () => {
+      expect(component).toBeTruthy();
+    });
+  
+    it("should have the correct Home Page Surprise Text", () => {
+      const data = fixture.nativeElement;
+      expect(data.querySelector('.homePageSurpriseText').textContent).toContain(HOMEPAGE_SURPRISE_TEXT);
+    });
+  
+    it("should have the correct Home Page Welcome Text", () => {
+      const data = fixture.nativeElement;
+      expect(data.querySelector('.homePageWelcomeText').textContent).toContain(HOMEPAGE_WELCOME_TEXT);
+    });
+  
+    it("should have the correct Home Page Click Text", () => {
+      const data = fixture.nativeElement;
+      expect(data.querySelector('.homePageClickText').textContent).toContain(HOMEPAGE_CLICK_TEXT);
+    });
+  
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
 
-  it("Home Page Surprise Text should be correct", () => {
-    expect(component.homePageSurpriseText).toEqual(HOMEPAGE_SURPRISE_TEXT);
+  describe('HomeComponent TypeScript', () => {
+    let component : HomeComponent;
+    beforeEach(() => {
+      component = new HomeComponent();
+    });
+  
+    it("should create", () => {
+      expect(component).toBeTruthy();
+    })
+  
+    it("should have the correct Home Page Surprise Text", () => {
+      expect(component.homePageSurpriseText).toEqual(HOMEPAGE_SURPRISE_TEXT);
+    });
+  
+    it("should have the correct Home Page Welcome Text", () => {
+      expect(component.homePageWelcomeText).toEqual(HOMEPAGE_WELCOME_TEXT);
+    });
+  
+    it("should have the correct Home Page Click Text", () => {
+      expect(component.homePageClickText).toEqual(HOMEPAGE_CLICK_TEXT);
+    });
+  
   });
+})
 
-  it("Home Page Welcome Text should be correct", () => {
-    expect(component.homePageWelcomeText).toEqual(HOMEPAGE_WELCOME_TEXT);
-  });
 
-  it("Home Page Click Text should be correct", () => {
-    expect(component.homePageClickText).toEqual(HOMEPAGE_CLICK_TEXT);
-  });
 
-  it("Home Component should have the correct Home Page Surprise Text in HTML", () => {
-    const data = fixture.nativeElement;
-    expect(data.querySelector('.homePageSurpriseText').textContent).toContain(HOMEPAGE_SURPRISE_TEXT);
-  });
-
-  it("Home Component should have the correct Home Page Welcome Text in HTML", () => {
-    const data = fixture.nativeElement;
-    expect(data.querySelector('.homePageWelcomeText').textContent).toContain(HOMEPAGE_WELCOME_TEXT);
-  });
-
-  it("Home Component should have the correct Home Page Click Text in HTML", () => {
-    const data = fixture.nativeElement;
-    expect(data.querySelector('.homePageClickText').textContent).toContain(HOMEPAGE_CLICK_TEXT);
-  });
-
-});
