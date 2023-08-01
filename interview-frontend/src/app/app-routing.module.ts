9370443bef3ef4f5b5e660cbc1df24941aa57fe7
baskayamert <baskaya_1999@hotmail.com>
2023-08-01 22:22:43 +0300

    Routing was refactored according to the query parameter logic.

--- interview-frontend/src/app/app-routing.module.ts
+++ interview-frontend/src/app/app-routing.module.ts
@@ -1,23 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CityComponent } from './components/city/city.component';
-import { AppComponent } from './app.component';
-import { PageNumberParameterGuard } from './guards/page-number-parameter.guard';
-import { SearchTextParameterGuard } from './guards/search-text-parameter.guard';
+// import { PageNumberParameterGuard } from './guards/page-number-parameter.guard';
+// import { SearchTextParameterGuard } from './guards/search-text-parameter.guard';
 import { HomeComponent } from './components/home/home.component';
+import { PageNumberParameterGuard } from './guards/page-number-parameter.guard';
 
 const routes: Routes = [
   { path: '', component: HomeComponent},
-  { path: 'cities/searchResult/:searchText/:pageNumber', component: CityComponent, canActivate: [SearchTextParameterGuard]},
-  { path: 'cities/:pageNumber', component: CityComponent, canActivate: [PageNumberParameterGuard] },
-  { path: 'cities/searchResult/:searchText', redirectTo: '/cities/searchResult/:searchText/1'},
-  { path: 'cities', redirectTo: '/cities/1'},
+  { path: 'cities', component: CityComponent, pathMatch: 'full', canActivate: [PageNumberParameterGuard]},
+  { path: 'cities', component: CityComponent, data: { queryParams: ['pageNumber', 'searchText'] } },
 
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
-  providers: [PageNumberParameterGuard, SearchTextParameterGuard]
+  providers: [PageNumberParameterGuard]
 })
 export class AppRoutingModule { }
